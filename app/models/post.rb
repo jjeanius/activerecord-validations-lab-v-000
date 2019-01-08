23a84bd62f80e2ea0f9d713336ac8346fe640a1a
Judy McGoldrick <jjeanius@verizon.net>
2019-01-08 05:02:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-#  validates :short_content, length: { minimum: 250}
+  validates :content, presence: true, length: { minimum: 250}
 
 end
+Post content is at least 250 characters long
+Post summary is a maximum of 250 characters
