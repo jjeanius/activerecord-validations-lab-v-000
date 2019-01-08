a6d3ca1410c93f274625e14bad494e2cc150dfbc
Judy McGoldrick <jjeanius@verizon.net>
2019-01-08 04:23:44 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :size, inclusion: (large)
+  validates :size, numericality: true, on: :large
   #validates :number, length: { minimum: 9}
 end
