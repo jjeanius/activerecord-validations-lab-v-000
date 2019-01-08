9dec6e5400b8d336079d8543585b8431adc8ca62
Judy McGoldrick <jjeanius@verizon.net>
2019-01-08 05:30:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
 
-  validator :valid_attrs, validates_with:
+  def validator_each(title, content, summary, category)
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 250}
   validates :summary, presence: true, length: { maximum: 250}
   validates :category, inclusion: {in: %w(Ficture  Non-Fiction)}
-
+end
 
 end
 # Post category is either Fiction or Non-Fiction This step requires
